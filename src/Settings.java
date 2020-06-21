305c6be1af03b6fd71e32b4f40333968e5f575b7
Teo Förste <teo-paul.foerste@informatik.hs-fulda.de>
2020-06-22 01:02:23 +0200

    Version 3

--- src/Settings.java
+++ src/Settings.java
@@ -1,6 +1,6 @@
 public class Settings {
 	
-	public static final String VERSION = "Version 2";
+	public static final String VERSION = "Version 3";
 	public static final String URL_REMOTE = "https://github.com/Oedeltroet/Updater.git";
 	public static final String FILE_TMP = "tmp";
 }
